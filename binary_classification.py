942e5b1d1a07563023ee13b37705ebfd5ff0223d
cryptoNice1 <95502003+cryptoNice1@users.noreply.github.com>
2021-12-10 17:28:46 -0500

    Update binary_classification.py

--- binary_classification.py
+++ binary_classification.py
@@ -12,17 +12,16 @@ from k_PLS import matrix_standardize
 class One_versus_one:
     """One versus one data structure"""
     
-    def __init__(self,f,c1, c2):
+    def __init__(self,df,c1, c2):
         """Initializes the 2 classes from the given dataset
         
-        Input:      f   - the filename
+        Input:      df  - the dataframe
                     c1  - the first class number
                     c2  - the second class number"""
         
         self.K = 2
         
         """Reading in data"""
-        df = pd.read_csv(f, delimiter = "\t", header = None)
         self.lab = np.array(df[0])
         
         """Creating new dataset with just the two classes"""
@@ -33,40 +32,35 @@ class One_versus_one:
         self.X0 = np.concatenate((class1, class2),axis=0)
         self.X0[:,0] = np.ones(sum(self.K0))
         self.X0 = matrix_standardize(self.X0)
-       
+        
     def __str__(self):
         print(pd.DataFrame(self.X0))
         print("K0:",self.K0)
         X = self.X0
         print("X0:",X.shape[0],"x",X.shape[1])
-        print("K")
+        print("K:",self.K)
         return""
         
-class One_Versus_All:
-    """A data structure for the classes"""
+class One_versus_all:
+    """A general data structure for the classes"""
     
-    def __init__(self,f,c1):
+    def __init__(self,df):
         """Initializes the 2 classes from the given class and the others
         
-        Input:      df  - a pandas dataframe representation of the data
-                    c1  - the class number of interest"""
+        Input:      f   - a pandas dataframe representation of the data"""
     
         self.K = 5
         
         """Reading in data"""
-        df = pd.read_csv(f, delimiter = "\t", header = None)
         self.lab = np.array(df[0])
         
         """Creating new dataset with just two classes"""
-        class1 = df[df[0] == c1].to_numpy()
-        class2 = df[df[0] != c1].to_numpy()
         self.K0 = []
         for i in range(1,6):
             self.K0.append(df[df[0] == i].to_numpy().shape[0])
+            
         self.K = 5
-        
-        self.X0 = np.concatenate((class1, class2),axis=0)
-
+        self.X0 = df.to_numpy()
         self.X0[:,0] = np.ones(sum(self.K0))
         self.X0 = matrix_standardize(self.X0)
         
@@ -75,5 +69,10 @@ class One_Versus_All:
         print("K0:",self.K0)
         X = self.X0
         print("X0:",X.shape[0],"x",X.shape[1])
-        print("K")
+        print("K:",self.K)
         return""
+
+if __name__ == "__main__":
+    f = "RamanData_Burn.txt"
+    print(One_versus_one(f,1,2))
+    print(One_versus_all(f))
